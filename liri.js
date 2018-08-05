96f334db06cd81fff5596a5b16644ebb452e9cdb
Karsten @ Ubuntu on Dell Latitude e6430s <karstenrabe91@gmail.com>
2018-08-05 18:05:40 -0400

    Add functions

--- liri.js
+++ liri.js
@@ -3,24 +3,44 @@
 var twitClient = require("./twitter.js");
 var spotClient = require("./spotify.js");
 var request = require("request");
+var fs = require("fs");
 
 console.log(`Twitter client: ${twitClient}`);
-console.log(twitClient);
 console.log(`Spotify client: ${spotClient}`);
-console.log(spotClient);
 
 funcs = {
     tweets: function () {
-
+        return "tweets";
     },
-    spotify: function () {
-
+    spotify: function (song) {
+        console.log(song);
+        return "spotty";
     },
-    movie: function () {
-
+    movie: function (movie) {
+        console.log(movie);
+        return "movies";
     },
     doIt: function () {
-
+        fs.readFile("./random.txt", "utf8", function (err, data) {
+            if (err) {
+                console.log(err);
+            }
+            else {
+                funcs.spotify(data);
+            }
+        });
+        return "do it!";
+    },
+    request: function(url, callback) {
+        console.log(`Making request to ${url}`);
+        console.log(`Firing callback ${callback}`);
+        return "fudge";
     }
 };
 
+if (process.argv[3]) {
+    console.log(funcs[process.argv[2]](process.argv[3]));
+}
+else if (process.argv[2]) {
+    console.log(funcs[process.argv[2]]());
+}
