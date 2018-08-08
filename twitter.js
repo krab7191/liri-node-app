5a6f247eb78014000830dabae4f5fc9a1aa0e920
Karsten @ Ubuntu on Dell Latitude e6430s <karstenrabe91@gmail.com>
2018-08-07 20:10:30 -0400

    Finished logging functionality

--- twitter.js
+++ twitter.js
@@ -2,23 +2,25 @@
 //jshint esversion: 6
 
 require("dotenv").config();
+var lf = require('./logger.js');
 var keys = require("./keys.js");
 var Twitter = require("twitter");
 var twitClient = new Twitter(keys.twitter);
 
 function getTweets() {
     twitClient.get('statuses/user_timeline', {screen_name: 'Dwitter89422817'}, function (error, tweets, response) {
+        var toLog = {};
         if (!error && response.statusCode === 200) {
             tweets.forEach(function(i) {
-                if (tweets.indexOf(i) > 19) {
-                    console.log(tweets.indexOf(i));
-                }
                 console.log(`${i.created_at}: "${i.text}"`);
+                toLog[tweets.indexOf(i)] = [i.created_at, i.text];
             });
         }
         else {
             console.log(`Error occurred: ${error}`);
+            toLog.Error = error;
         }
+        lf.fileLog(JSON.stringify(toLog) + "\n");
     });
 }
 
